ce254bd92b5c994857f335fc8953a6ca70d05c69
andreplayer1 <andreplayer1@gmail.com>
2023-04-10 10:45:11 -0300

    Final da aula 14 de JS
    Aprendizados com laços de repetição
    while
    do while
    for
    
    um pouco sobre depuração no VS Code

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,15 @@
+/* do while - Teste logíco no final
 var c = 1;
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c <= 6)
+*/
 
-/*var c = 1;
-while (c <= 6){
+
+/* while - Teste lógico no incio
+var c = 1;
+while (c <= 6){    
     console.log(`Passo ${c}`)
     c++
 }
