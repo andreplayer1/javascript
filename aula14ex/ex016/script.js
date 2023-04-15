a1fd387766f483e95a0acf2d04cc31b8b6273302
andreplayer1 <andreplayer1@gmail.com>
2023-04-14 22:12:24 -0300

    Ex 15
    Loops em JS

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,22 +1,32 @@
 function contar(){
-    var ini = document.getElementById('txti');
-    var fim = document.getElementById('txtf');
-    var passo = document.getElementById('txtp');
+    let ini = document.getElementById('txti');
+    let fim = document.getElementById('txtf');
+    let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
 
-    var res = document.getElementById('res');
-    
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados');
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!';
+        //window.alert ('[ERRO] Faltam dados.');
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando...';
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
-
-        for (let c = i; c <= f; c += p){
-            res.innerHTML += ` ${c[
-                
-            ]} `
+        if (p <=0) {
+            window.alert('Passo invalido! Considerando PASSO 1');
+            p = 1
         }
+        if (i < f) {
+            // Contagem Crescente
+        for (let c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+        } else {
+            // Contagem Regressiva
+            for(let c = i; c >= f; c-= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
